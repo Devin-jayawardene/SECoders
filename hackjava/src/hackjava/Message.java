10eaf4d8dd96dd2bfa073ef8449cb4b01c9d0c39
NethminiDevyanjalee <73274982+NethminiDevyanjalee@users.noreply.github.com>
2021-10-23 12:38:11 +0530

    integrade message class

---
+++ hackjava/src/hackjava/Message.java
@@ -0,0 +1,7 @@
+package hackjava;
+
+public class Message {
+    public static void main(String[] args){
+        System.out.println("Hello welcome, Your conversations are end to end encrypted");
+    }
+}
